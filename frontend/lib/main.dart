f110ed77ddab76d282867fcddc05528e4edb9172
Ivan <77185900+wpinrui@users.noreply.github.com>
2024-07-30 19:02:42 +0800

    Update title in main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -11,7 +11,7 @@ class ChattleMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chattle',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
