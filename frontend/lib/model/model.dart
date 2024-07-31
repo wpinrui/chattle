b4bf096261735792e3061579553bdb5a8fe053e5
wpinrui <77185900+wpinrui@users.noreply.github.com>
2024-07-31 15:36:44 +0800

    Add model init

--- frontend/lib/model/model.dart
+++ frontend/lib/model/model.dart
@@ -3,6 +3,20 @@ import 'package:chattle/data/types.dart';
 class Model {
   static final List<MessageData> _messages = []; // assume always sorted by timestamp
 
+  static void init() async {
+    // Load messages from storage
+    // TODO: Implement this. For now, just add a welcome message
+    _messages.add(
+      MessageData(
+        id: '0',
+        message: 'Hello! I\'m Janet. What can I do for you?',
+        sender: Person.bot,
+        receiver: Person.user,
+        timestamp: DateTime.now(),
+      )
+    );
+  }
+
   static List<MessageData> get messages => _messages;
 
   static void addMessage(MessageData message) {
