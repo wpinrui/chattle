9e784e9538a041ebdafefbeff8e577af7f7a0593
wpinrui <77185900+wpinrui@users.noreply.github.com>
2024-08-01 18:07:05 +0800

    Create all_chats page

---
+++ frontend/lib/ui/all_chats/all_chats.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AllChats extends StatefulWidget {
+  const AllChats({super.key});
+
+  @override
+  State<AllChats> createState() => _AllChatsState();
+}
+
+class _AllChatsState extends State<AllChats> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
